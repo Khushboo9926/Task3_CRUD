64f677c003bf3847ba9008d10a6081c3d4c6bdf8
nirmalkoffice <77260962+nirmalkoffice@users.noreply.github.com>
2021-06-14 21:28:15 +0530

    first commit from khushboo

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'task-two'`, () => {
+  it(`should have as title 'routingDemo'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('task-two');
+    expect(app.title).toEqual('routingDemo');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('task-two app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('routingDemo app is running!');
   });
 });
